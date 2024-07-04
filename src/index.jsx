e6cb86baddd130c01e3d93bd6b35748b4b38114c
mohnapne <kuvaldin2012@gmail.com>
2024-07-04 10:26:42 +0200

    lesson 2

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import './index.css';
+import Lesson01 from './lessons/lesson_01/Lesson01';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <App />
+    <Lesson01 />
   </>
 );
