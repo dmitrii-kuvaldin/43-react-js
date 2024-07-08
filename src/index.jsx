1e1a3c233743a6f165326f8ca72be7009b43da50
mohnapne <kuvaldin2012@gmail.com>
2024-07-08 13:48:33 +0200

    useState

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './app/App';
-import './index.css';
-import MyForm from './components/myForm/MyForm';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app/App";
+import "./index.css";
+import Counter from './components/counter/Counter';
 // import Lesson01 from './lessons/lesson_01/Lesson01';
 // import Lesson02 from './lessons/lesson_02/Lesson02';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <App />
     {/* <Lesson01 /> */}
     {/* <Lesson02/> */}
-    <MyForm/>
+    {/* <MyForm/> */}
+    <Counter />
   </>
 );
